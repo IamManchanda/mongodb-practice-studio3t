6f64736b312737a5da43e0109d54dc411701d047
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-04-10 17:59:57 +0530

    Projection with Arrays - Slice and length!

---
+++ projection-length.js
@@ -0,0 +1,10 @@
+db.friends.aggregate([
+  {
+    $project: {
+      _id: 0,
+      numScores: {
+        $size: "$examScores",
+      },
+    },
+  },
+]);
---
+++ projection-slice.js
@@ -0,0 +1,10 @@
+db.friends.aggregate([
+  {
+    $project: {
+      _id: 0,
+      examScore: {
+        $slice: ["$examScores", 2, 1],
+      },
+    },
+  },
+]);
