16d6ea0814cfc4d602c9837a1ad762a0a8c85cb7
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-04-11 20:48:34 +0530

    practice!

---
+++ additional-stages.js
@@ -0,0 +1,30 @@
+db.persons.aggregate([
+  {
+    $match: {
+      gender: "male",
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      gender: 1,
+      name: {
+        $concat: ["$name.title", " ", "$name.first", " ", "$name.last"],
+      },
+      birthdate: {
+        $toDate: "$dob.date",
+      },
+    },
+  },
+  {
+    $sort: {
+      birthdate: 1,
+    },
+  },
+  {
+    $skip: 10,
+  },
+  {
+    $limit: 10,
+  },
+]);
---
+++ transformed-persons-geonear.js
@@ -0,0 +1,27 @@
+/* 
+db.transformedPersons.createIndex({
+  location: "2dsphere",
+});
+*/
+
+db.transformedPersons.aggregate([
+  {
+    $geoNear: {
+      near: {
+        type: "Point",
+        coordinates: [-18.4, -42.8],
+      },
+      maxDistance: 1000000,
+      num: 10,
+      query: {
+        age: {
+          $gt: 30,
+        },
+      },
+      distanceField: "distance",
+    },
+  },
+  {
+    $limit: 10,
+  },
+]);
